8215493aa0002a733a0d658198f9f5e852921efc
Phương Oanh Bùi <oanhbui@Phuongs-MacBook-Pro.local>
2022-04-05 23:02:40 -0700

    Components Part 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
